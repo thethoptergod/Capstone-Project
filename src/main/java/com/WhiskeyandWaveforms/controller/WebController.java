087cd2389bfe0a3336f179a1fb3d0ed15c8c54bb
thethoptergod <93736743+thethoptergod@users.noreply.github.com>
2022-02-26 16:32:44 -0500

    Initial commit

---
+++ src/main/java/com/WhiskeyandWaveforms/controller/WebController.java
@@ -0,0 +1,12 @@
+package com.WhiskeyandWaveforms.controller;
+
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//public class WebController {
+//@RequestMapping("/test")
+//String test() {
+//	return "Hello! This is a test!";
+//}
+//}
---
+++ src/test/java/com/WhiskeyandWaveforms/main/WhiskeyandWaveformsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WhiskeyandWaveforms.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiskeyandWaveformsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
